0342cf194f096e129bb642978ea2eaefe8100b1c
Parikshit <parikshitgupta2001@gmail.com>
2024-11-06 13:14:55 +0530

    Configure fonts and color in tailwind

--- app/index.tsx
+++ app/index.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const index = () => {
   return (
     <View className="flex items-center justify-center h-screen">
-      <Text className="text-2xl font-bold">index</Text>
+      <Text className="text-2xl font-">index</Text>
     </View>
   );
 };
